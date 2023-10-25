bf667697cc6ede74f9df0b3936f7e7a2ac411fe2
Venkata Raviteja Gullapudi <venkataravitejagullapudi@MacBook-Air-de-Venkata.local>
2023-10-26 00:53:52 +0530

    react func comps & jsx

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Creating a element is a core thing of react, so it comes from the react library.
-const heading = React.createElement("h1", { id: "heading" }, "Hello world from React")
+//JSX -> Babel transpiles it to React.createElement -> JS object -> HtmlElement
+const heading = <h1 className="head">Namaste React using JSX 🚀</h1>;
+console.log(heading);
 
-// Here React.createElement return(heading) is an object specifying the element
-// Props - Attributes + children
+const Title = ()=> {
+    return <h1>React Title</h1>
+}
 
-// For creating nested elements like <div><h1></h1></div>
-let parent = React.createElement("div", { id: "parent" }, heading)
-
-// Siblings and child
-
-parent = React.createElement("div", { id: "parent" }, [heading, heading])
+// React Functional Component
+const HeadingComponent = () => {
+  return <div>
+    {heading}
+    {/* Child component */}
+    <Title/>
+    {/* Title can also be written like this */}
+    {Title()}
+  </div>
+};
 
 // If we want to create multiple elements with lot of sibilings
-// this will make it complex. So there JSX comes into picture.
+// this will make it complex So there JSX comes into picture.
 
 // Creating a root and rendering react elements into it is a job of react-dom library
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
+``;
 // Takes the element object and based on the object, render method will convert to the html tag and put into the root
 // root.render(heading);
-root.render(parent);
+root.render(<HeadingComponent/>);
--- Readme.md
+++ Readme.md
@@ -18,4 +18,4 @@
 - Differential Bundling - Support for legacy browsers 
 - Consistent Hashing - Means that the same file will always have the same name, allowing for better caching. Also, if you change a file, only that file will have a different name, so the browser will only download that file. This is a huge improvement over the traditional way of bundling, where every file has a different name every time you change something, so the browser has to download everything again.
 - Automatic Polyfilling - Parcel automatically detects which browsers your users are using and only sends the necessary polyfills. This means that your users will download less code, and your app will load faster.
-- Production Builds - Parcel can create production builds with code minification and asset optimization out of the box, no need to install additional plugins or bundles.
+- Production Builds - Parcel can create production builds with code minification and asset optimization out of the box, no need to install additional plugins or bundles.s
